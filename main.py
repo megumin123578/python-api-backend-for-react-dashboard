1ffc792ece6766251149161ccc8a22f6e7bb6ab5
megumin123578 <saranghae123578@gmail.com>
2025-12-01 14:49:29 +0700

    update code base

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from routes.traffic_timeseries import router as ts_router
 
 app = FastAPI()
 
-# (tuỳ môi trường) bật CORS cho frontend dev
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000", "http://localhost:5173"],
